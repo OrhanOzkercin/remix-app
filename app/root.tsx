1e87f70b88bc6784e605c4af67051a9c02b2ff1b
Orhan Özkerçin <orhanozkercin@yahoo.com>
2025-01-26 00:56:41 +0300

    root

--- app/root.tsx
+++ app/root.tsx
@@ -1,28 +1,14 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+import styles from "./styles/tailwind.css?url";
+import Header from "./components/header";
+import { FocusModeProvider } from "./contexts/focus-mode-context";
+import { Toaster } from "./components/ui/toaster";
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export default function App() {
   return (
     <html lang="en">
       <head>
@@ -31,15 +17,20 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
-        {children}
+      <body className="min-h-screen bg-background font-sans antialiased">
+        <FocusModeProvider>
+          <div className="relative flex min-h-screen flex-col">
+            <Header />
+            <main className="flex-1">
+              <Outlet />
+            </main>
+          </div>
+        </FocusModeProvider>
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
+        <Toaster />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
